f927bff3e1b754b307eefac63edc11552abf4819
Muthukumar-Palaniyappan <muthukumar.palaniyappan@yahoo.com.au>
2019-06-13 17:04:09 +1000

    Initial Checkin For Portfolio Site using React. Router Implemented.

---
+++ myportfoliosite/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl';
+import Main from './Components/main';
+import {Link} from 'react-router-dom';
+function App() {
+  return (
+    <div className="demo-big-content">
+    <Layout fixedHeader>
+        <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+            <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/contact">Contact Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/contact">Contact Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+            </Navigation>
+        </Drawer>
+        <Content >
+          <Main/>
+          </Content>
+    </Layout>
+</div>
+  );
+}
+
+export default App;
---
+++ myportfoliosite/src/Components/landingpage.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import {Grid,Cell} from 'react-mdl';
+
+class LandingPage extends Component{
+    render(){
+        return(
+            <div style={{width:'100%',margin:'auto'}}>
+                <Grid>
+                    <Cell>
+                        LandingPage
+                    </Cell>
+                </Grid>
+
+</div>
+        )
+    }
+}
+
+export default LandingPage;
+    
